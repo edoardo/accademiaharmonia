f2b59b47609517552fad1ca191e88402688151d5
Edoardo Sabadelli <edoardo@cpan.org>
2010-10-24 17:08:42 +0200

    Updated the page for the maestro, regenerated all the italian pages

--- generator.pl
+++ generator.pl
@@ -20,34 +20,33 @@ my $cHtmlPath = 'html/'.$cLanguage;
 
 # Website structure
  my @aWebsite = (
-#   { page => 'home', title => { it => 'Presentazione', en => 'Presentation' } },
-#   { page => 'choir', title => { it => 'il Coro', en => 'the Choir' } },
-#   { page => 'repertory', title => { it => 'il Repertorio', en => 'the Repertory' } },
-#   { page => 'giuseppe_schiff', title => { it => 'Giuseppe Schiff', en => 'Giuseppe Schiff' } },
-#   { page => 'beppino_delle_vedove', title => { it => 'Beppino Delle Vedove', en => 'Beppino delle Vedove' } },
-#   { page => 'publications', title => { it => 'le Pubblicazioni', en => 'the Publications' } },
-#   { page => 'events', title => { it => 'Eventi', en => 'Events' } },
-#   { page => 'logo', title => { it => 'Logo', en => 'Logo' } },
+    { page => 'home', title => { it => 'Presentazione', en => 'Presentation' } },
+    { page => 'choir', title => { it => 'il Coro', en => 'the Choir' } },
+    { page => 'repertory', title => { it => 'il Repertorio', en => 'the Repertory' } },
+    { page => 'paola_gasparutti', title => { it => 'Paola Gasparutti', en => 'Paola Gasparutti' } },
+    { page => 'giuseppe_schiff', title => { it => 'Giuseppe Schiff', en => 'Giuseppe Schiff' } },
+    { page => 'beppino_delle_vedove', title => { it => 'Beppino Delle Vedove', en => 'Beppino delle Vedove' } },
+    { page => 'publications', title => { it => 'le Pubblicazioni', en => 'the Publications' } },
+    { page => 'events', title => { it => 'Eventi', en => 'Events' } },
+    { page => 'logo', title => { it => 'Logo', en => 'Logo' } },
     { page => 'contacts', title => { it => 'Contatti', en => 'Contacts' } },
-#   { page => 'about', title => { it => 'Informazioni', en => 'Informations' } },
+    { page => 'about', title => { it => 'Informazioni', en => 'Informations' } },
 #   { page => 'mailinglist', title => { it => 'Mailing list', en => 'Mailing list' } },
 );
 
 # Get next event (show in all pages)
 my $cNextEvent;
 
-=cut
-my $rItems = feed::getItems( feed::parse( "rss/$cLanguage/harmonia.rss" ) );
-if( ref $rItems eq 'ARRAY' )
-{
-    my $nIndex = 0;
-    $cNextEvent =
-        sprintf( '<a href="events.html%s"><b><i>%s</i></b> - %s</a>',
-            map { encode_entities( $_ ) } ( substr( $rItems->[$nIndex]->{'link'}, index( $rItems->[$nIndex]->{'link'}, '#' ) ),
-                                            substr( $rItems->[$nIndex]->{'title'}, 0, 10 ), substr( $rItems->[$nIndex]->{'title'}, 13 ) )
-        );
-}
-=cut
+#my $rItems = feed::getItems( feed::parse( "rss/$cLanguage/harmonia.rss" ) );
+#if( ref $rItems eq 'ARRAY' )
+#{
+#    my $nIndex = 0;
+#    $cNextEvent =
+#        sprintf( '<a href="events.html%s"><b><i>%s</i></b> - %s</a>',
+#            map { encode_entities( $_ ) } ( substr( $rItems->[$nIndex]->{'link'}, index( $rItems->[$nIndex]->{'link'}, '#' ) ),
+#                                            substr( $rItems->[$nIndex]->{'title'}, 0, 10 ), substr( $rItems->[$nIndex]->{'title'}, 13 ) )
+#        );
+#}
 
 my @aLocaltime = localtime();
 
